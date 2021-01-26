45dde9d1b6eb390ab330ea789f525a0a7a7df2d1
Lawdut <tprigent@hotmail.fr>
2021-01-26 13:18:57 +0100

    modif range

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -7,6 +7,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+//use Symfony\Component\Validator\Constraints\Lenght;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity(repositoryClass=AnimalRepository::class)
  */
@@ -36,6 +39,7 @@ class Animal
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Range(min=2,max=30)
      */
     private $poids;
 
